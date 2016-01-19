df231b5c2938a73995b8cd468c57bdc3c05260e9
Lewcio <Lewy@MacBook-Pro-Michal.local>
2016-01-19 01:16:36 +0100

    font

--- public/js/app.js
+++ public/js/app.js
@@ -68,53 +68,6 @@ rocket.register.module('content', function (element, params) {
 
 'use strict';
 
-rocket.register.module('top', function (element, params) {
-
-  var $money;
-  var $students;
-  var $profit;
-
-  return {
-    constructor : function () {
-      var $this = this;
-      $this.setup();
-
-      rocket.register.event('updateMoney', function(money) {
-        $this.update(money);
-      });
-
-      rocket.register.event('updateStudents', function (students) {
-        $this.updateStudents(students);
-      });
-
-      rocket.register.event('updateProfit', function (profit) {
-        $this.updateProfit(profit);
-      });
-
-    },
-    update : function(money) {
-      $money.innerHTML = money;
-      localStorage.setItem('money', money);
-    },
-    updateStudents : function (students) {
-      $students.innerHTML = students;
-      localStorage.setItem('students', students);
-    },
-    updateProfit : function (profit) {
-      $profit.innerHTML = ['(+', profit, '/s)'].join('');
-      localStorage.setItem('proft', profit);
-
-    },
-    setup : function () {
-      $students = element.querySelector('p:nth-child(1) > span');
-      $money    = element.querySelector('p:nth-child(2) > span');
-      $profit   = element.querySelector('p:nth-child(2) > i');
-    }
-  }
-});
-
-'use strict';
-
 rocket.register.service("engine", function () {
 
   function parse(name, def) {
@@ -1229,6 +1182,53 @@ rocket.register.service('tomaszewXP', function() {
 
     }
   };
+});
+
+'use strict';
+
+rocket.register.module('top', function (element, params) {
+
+  var $money;
+  var $students;
+  var $profit;
+
+  return {
+    constructor : function () {
+      var $this = this;
+      $this.setup();
+
+      rocket.register.event('updateMoney', function(money) {
+        $this.update(money);
+      });
+
+      rocket.register.event('updateStudents', function (students) {
+        $this.updateStudents(students);
+      });
+
+      rocket.register.event('updateProfit', function (profit) {
+        $this.updateProfit(profit);
+      });
+
+    },
+    update : function(money) {
+      $money.innerHTML = money;
+      localStorage.setItem('money', money);
+    },
+    updateStudents : function (students) {
+      $students.innerHTML = students;
+      localStorage.setItem('students', students);
+    },
+    updateProfit : function (profit) {
+      $profit.innerHTML = ['(+', profit, '/s)'].join('');
+      localStorage.setItem('proft', profit);
+
+    },
+    setup : function () {
+      $students = element.querySelector('p:nth-child(1) > span');
+      $money    = element.querySelector('p:nth-child(2) > span');
+      $profit   = element.querySelector('p:nth-child(2) > i');
+    }
+  }
 });
 
 'use strict';
