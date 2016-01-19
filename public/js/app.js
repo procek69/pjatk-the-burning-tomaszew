c89f7d5150f72568ac52a73f07c6a3eb018e7609
Lewcio <Lewy@172.23.65.215.wi-fi.pjwstk.edu.pl>
2016-01-19 10:35:35 +0100

    money in xp

--- public/js/app.js
+++ public/js/app.js
@@ -986,7 +986,7 @@ rocket.register.service('tomaszewXP', function() {
           'info' : '+2 kodu',
           'click' : function () {
             rocket.service('engine').change({
-              'kod' : 2
+              'money' : 2
             });
           }
         }
@@ -1281,6 +1281,23 @@ rocket.register.module("left/default", function(element, params) {
 
 'use strict';
 
+rocket.register.module('content/tile/upgrade', function (element, params) {
+
+  element.querySelector('div.visible span').innerHTML = params.name;
+  element.querySelector('div.visible i').innerHTML = [params.koszt, 'kodu'].join(' ');
+  element.querySelector('div.hidden p').innerHTML = params.info;
+
+  return {
+    constructor : function () {
+      element.addEventListener('click', function (e) {
+        params.click(e.target);
+      }, true);
+    }
+  }
+});
+
+'use strict';
+
 rocket.register.module('content/tile/skill', function (element, params) {
 
   element.querySelector('div.visible span').innerHTML = params.name;
@@ -1308,21 +1325,4 @@ rocket.register.module('content/tile/skill', function (element, params) {
     }
   }
 
-});
-
-'use strict';
-
-rocket.register.module('content/tile/upgrade', function (element, params) {
-
-  element.querySelector('div.visible span').innerHTML = params.name;
-  element.querySelector('div.visible i').innerHTML = [params.koszt, 'kodu'].join(' ');
-  element.querySelector('div.hidden p').innerHTML = params.info;
-
-  return {
-    constructor : function () {
-      element.addEventListener('click', function (e) {
-        params.click(e.target);
-      }, true);
-    }
-  }
 });
