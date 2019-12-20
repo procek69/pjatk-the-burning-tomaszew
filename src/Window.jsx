7dfb265e90f552cca815216973c910aae3e7e764
procek69 <llukas.de@gmail.com>
2019-12-20 15:24:39 +0100

    tomaszew95 Window component

--- src/Window.jsx
+++ src/Window.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import Draggable from 'react-draggable';
 
-export default function ({ children }) {
+const PROCEK_CONST = 0.69;
+
+const getRandomPointInScreen = () => {
+    let {height, width} = window.screen;
+    if (!height || !width) {
+        return {x: 0, y: 0};
+    }
+    return {
+        x: Math.random() * width * PROCEK_CONST,
+        y: Math.random() * height * PROCEK_CONST,
+    }
+}
+
+export default function ({ children, title }) {
+
+    const startPosition = getRandomPointInScreen();
+
     return (
-        <Draggable>
-            <div>I can now be moved around!</div>
-            <div>{children}</div>
+        <Draggable defaultPosition={startPosition}>
+            <div className="window">
+                <div className="titleBar">
+                    <p>{title || "Window"}</p>
+                    <div className="divider"></div>
+                    <button>X</button>
+                </div>
+                {children}
+            </div>
         </Draggable>
     );
 }
